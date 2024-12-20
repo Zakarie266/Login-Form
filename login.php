3cd6ff1126eb321fdc6e7166ba9c881c745c04ae
Zakarie266 <sakariye266@gmail.com>
2024-12-20 17:39:09 +0300

    Login Form

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Darusalaam Village</title>
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+	<div class="login-box">
+		<img src="avatar.png" class="avatar">
+		<h1>Login</h1>
+		<form action="check_login.php" method="post">
+			<p>Username</p>
+			<input type="text" name="username" placeholder="Enter Username" autocomplete="off">
+			<p>Password</p>
+			<input type="password" name="password" placeholder="Enter Password">
+			<input type="submit" name="submit" value="Login" >
+		</form>
+	</div>
+</body>
+</html>
